8f02f99825ed804de22e684e041b001636214194
Your Name <you@example.com>
2022-11-03 08:19:17 +0700

    fix ss9

--- module_3/src/ss9/exercise/product_discount_calculator/src/main/java/ProductCalculatorServlet.java
+++ module_3/src/ss9/exercise/product_discount_calculator/src/main/java/ProductCalculatorServlet.java
@@ -14,18 +14,19 @@ public class ProductCalculatorServlet extends HttpServlet {
 
         String productDescription = request.getParameter("product_description");
         double listPrice = Double.parseDouble(request.getParameter("list_price"));
-        BigDecimal listPrice1 = new BigDecimal(listPrice);
+        BigDecimal listPriceBig = new BigDecimal(listPrice);
         double discountPercent = Double.parseDouble((request.getParameter("discount_percent")));
-        BigDecimal discountPercent1 = new BigDecimal(discountPercent);
+        BigDecimal discountPercentBig = new BigDecimal(discountPercent);
         BigDecimal discountAmount = new BigDecimal(listPrice * discountPercent * 0.01);
         BigDecimal discountPrice = new BigDecimal(listPrice + (listPrice * discountPercent * 0.01));
 
         request.setAttribute("product_description", productDescription);
-        request.setAttribute("list_price", listPrice1);
-        request.setAttribute("discount_percent", discountPercent1);
+        request.setAttribute("list_price", listPriceBig);
+        request.setAttribute("discount_percent", discountPercentBig);
         request.setAttribute("discountAmount", discountAmount);
         request.setAttribute("discountPrice", discountPrice);
         request.getRequestDispatcher("/display.jsp").forward(request, response);
+//        response.sendRedirect("/display.jsp");
 
     }
 
